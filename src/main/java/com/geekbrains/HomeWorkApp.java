b59fbb0bc120dc5b5b0fc6c71fd8fbc30ba84432
msvdev-k <msvdev.k@gmail.com>
2021-11-04 11:45:44 +0300

    add HomeWorkApp

---
+++ src/main/java/com/geekbrains/HomeWorkApp.java
@@ -0,0 +1,68 @@
+package com.geekbrains;
+
+public class HomeWorkApp {
+
+    public static void main(String[] args) {
+
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+
+    }
+
+    public static void printThreeWords() {
+
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+
+    }
+
+    public static void checkSumSign() {
+
+        int a = 12;
+        int b = 45;
+
+        int sum = a + b;
+
+        if (sum >= 0) {
+            System.out.println("Сумма положительная");
+        }
+        else {
+            System.out.println("Сумма отрицательная");
+        }
+
+    }
+
+    public static void printColor() {
+
+        int value = 3;
+
+        if (value <= 0) {
+            System.out.println("Красный");
+        }
+        else if (value <= 100) {
+            System.out.println("Жёлтый");
+        }
+        else {
+            System.out.println("Зелёный");
+        }
+
+    }
+
+    public static void compareNumbers() {
+
+        int a = 20;
+        int b = -23;
+
+        if (a >= b) {
+            System.out.println("a >= b");
+        }
+        else {
+            System.out.println("a < b");
+        }
+
+    }
+
+}
